c61ca2a5a5b4a0e09e7fdb44b8bf594f0edbd2dd
Romain Tempo <romain.tempo@etu.univ-lyon1.fr>
2016-10-06 17:01:53 +0200

    [FEAT] added Main fragment, view and presenter

--- app/src/main/java/com/medsic/ttstowav/ui/activity/MainActivity.java
+++ app/src/main/java/com/medsic/ttstowav/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.medsic.ttstowav;
+package com.medsic.ttstowav.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.medsic.ttstowav.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/medsic/ttstowav/ui/presenter/MainPagePresenter.java
@@ -0,0 +1,31 @@
+package com.medsic.ttstowav.ui.presenter;
+
+import com.medsic.ttstowav.ui.view.MainPageView;
+
+/**
+ * Created by romaintempo on 06/10/2016.
+ */
+
+public class MainPagePresenter {
+    private MainPageView mainPageView;
+
+    public MainPagePresenter(MainPageView mainPageView) {
+        this.mainPageView = mainPageView;
+    }
+
+    public void saveNewPitchValue(int progress) {
+
+    }
+
+    public void saveNewSpeechrateValue(int progress) {
+
+    }
+
+    public void testSpeech(String text) {
+        
+    }
+
+    public void recordSpeech(String text) {
+
+    }
+}
